64f7e784788126a4aa84c8782dcb439212ba5e7b
andela-esitati <elsis.sitati@andela.com>
2017-02-27 22:00:19 +0300

    Finish User Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+
     else
       render 'new'
     end
