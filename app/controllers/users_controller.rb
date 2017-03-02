b8639566d2e41f29bf0643e8d7deb913e663fa3c
andela-esitati <elsis.sitati@andela.com>
2017-03-02 16:05:04 +0300

    add edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,32 +1,32 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
-     @user = User.new
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to user_url(@user)
-
+      redirect_to @user
     else
       render 'new'
     end
   end
 
-  # method that returns the parameters we want to allow
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
 end
