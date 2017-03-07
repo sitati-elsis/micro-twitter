44743ace42148b2d7857a0784a4d60c0ec9aa936
andela-esitati <elsis.sitati@andela.com>
2017-03-07 17:57:34 +0300

    list all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -39,8 +43,10 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
 
+  # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
